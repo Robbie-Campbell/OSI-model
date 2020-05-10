9c8ed9d52a9bf6176525b82a7f57d6a0fbc98b81
LFCRab <56073739+LFCRab@users.noreply.github.com>
2020-05-10 16:33:13 +0100

    Made it alot nicer to look at the code

--- app.js
+++ app.js
@@ -13,62 +13,20 @@ const presentation = document.querySelector(".presentation");
 const applicationText = document.querySelector(".application h1");
 const application = document.querySelector(".application");
 
-function textChange(){
-    physical.addEventListener("mouseenter", ()=>{
+function textChange(osiLayer, text, textChange){
+    osiLayer.addEventListener("mouseover", ()=>{
         setInterval(() => {
-        physicalText.textContent="The physical layer of the OSI model handles the hardware aspect of sending information.";
-            physicalText.classList.add("textChange");
-            physicalText.style.animation = "enter 0.4s";
+        textChange.textContent = String(text);
+            textChange.classList.add("textChange");
+            textChange.style.animation = "enter 0.4s";
         }, 1000);
     })
-    data.addEventListener("mouseover", ()=>{
-        setInterval(() => {
-        dataText.textContent="The data link layer uses MAC addresses to determine where the information needs to go, the senders MAC address is kept here for when the user sends data back.";
-           dataText.classList.add("textChange");
-           dataText.style.animation = "enter 0.4s";
-        }, 1000);
+};
 
-})
-   networking.addEventListener("mouseover", ()=>{
-        setInterval(() => {
-        networkingText.textContent="The networking layer uses the IP address to make sure that the correct client is recieving the data.";
-            networkingText.classList.add("textChange");
-            networkingText.style.animation = "enter 0.4s";
-        }, 1000);
-
-})
-    transport.addEventListener("mouseover", ()=>{
-        setInterval(() => {
-        transportText.textContent="The transport layer breaks the information into packets and transmits the data using TCP.";
-            transportText.classList.add("textChange");
-            transportText.style.animation = "enter 0.4s";
-        }, 1000);
-
-})
-    session.addEventListener("mouseover", ()=>{
-        setInterval(() => {
-        sessionText.textContent="The session layer attempts to set up a connection between the client and the server, though most applications do this anyways.";
-            sessionText.classList.add("textChange");
-            sessionText.style.animation = "enter 0.4s";
-        }, 1000);
-
-})
-    presentation.addEventListener("mouseover", ()=>{
-        setInterval(() => {
-        presentationText.textContent="The presentation layer ensures that the recieved data can be decrypted for the user in a usable format.";
-            presentationText.classList.add("textChange");
-            presentationText.style.animation = "enter 0.4s";
-        }, 1000);
-
-})
-    application.addEventListener("mouseover", ()=>{
-        setInterval(() => {
-        applicationText.textContent="The application layer is where the data is decompressed into a readable format for the user, normally through HTTP.";
-            applicationText.classList.add("textChange");
-            applicationText.style.animation = "enter 0.4s";
-        }, 1000);
-
-})
-
-}
-textChange();
+textChange(physical, "The physical layer of the OSI model handles the hardware aspect of sending information.", physicalText);
+textChange(data, "The data link layer uses MAC addresses to determine where the information needs to go, the senders MAC address is kept here for when the user sends data back.", dataText )
+textChange(networking, "The networking layer uses the IP address to make sure that the correct client is recieving the data.", networkingText);
+textChange(transport, "The transport layer breaks the information into packets and transmits the data using TCP.", transportText);
+textChange(session, "The session layer attempts to set up a connection between the client and the server, though most applications do this anyways.", sessionText);
+textChange(presentation, "The presentation layer ensures that the recieved data can be decrypted for the user in a usable format.", presentationText);
+textChange(application, "The application layer is where the data is decompressed into a readable format for the user, normally through HTTP.", applicationText);
--- index.php
+++ index.php
@@ -5,21 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>The OSI model</title>
 </head>
 <body>
     <nav>
-    <div class="heading"><h1>The OSI Model</h1></div>
-    <div class="flexPage"><a href="flex.html"><p>Flex</p></a></div>
+        <h1 class="text-center pt-3">The OSI Model</h1>
+        <p class="text-center text-muted">By Rab</p>
     </nav>
     <Main>
         <div class="model">
-            <div class="physical"><h1>The physical layer</h1></div>
-            <div class="dataLink"><h1>The data link layer</h1></div>
-            <div class="networking"><h1>The networking layer</h1></div>
-            <div class="transport"><h1>The transport layer</h1></div>
-            <div class="session"><h1>The session layer</h1></div>
-            <div class="presentation"><h1>The presentation layer</h1></div>
-            <div class="application"><h1>The application layer</h1></div>
+            <div class="physical box"><h1>The physical layer</h1></div>
+            <div class="dataLink box"><h1>The data link layer</h1></div>
+            <div class="networking box"><h1>The networking layer</h1></div>
+            <div class="transport box"><h1>The transport layer</h1></div>
+            <div class="session box"><h1>The session layer</h1></div>
+            <div class="presentation box"><h1>The presentation layer</h1></div>
+            <div class="application box"><h1>The application layer</h1></div>
         </div>
     </Main>
     <script src="app.js"></script>
